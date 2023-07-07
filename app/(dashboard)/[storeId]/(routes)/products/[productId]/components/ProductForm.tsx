523fc399e5150e44d275c5ee6506eb30cafed11e
khanhthanhdev <thanhkt27507@gmail.com>
2023-07-07 15:04:06 +0700

    update fix

--- app/(dashboard)/[storeId]/(routes)/products/[productId]/components/ProductForm.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/[productId]/components/ProductForm.tsx
@@ -36,10 +36,9 @@ import { Checkbox } from "@/components/ui/checkbox";
 
 interface ProductFormProps {
   initialData:
-    | (Product & {
+    Product & {
         images: Image[];
-      })
-    | null;
+      } | null;
   categories: Category[];
   sizes: Size[];
   colors: Color[];
@@ -100,7 +99,7 @@ export const ProductForm: React.FC<ProductFormProps> = ({
       setLoading(true);
       if (initialData) {
         await axios.patch(
-          `/api/${params.storeId}/products/${params.billboardId}`,
+          `/api/${params.storeId}/products/${params.productId}`,
           data
         );
       } else {
@@ -121,11 +120,11 @@ export const ProductForm: React.FC<ProductFormProps> = ({
     try {
       setLoading(true);
       await axios.delete(
-        `/api/${params.storeId}/billboards/${params.billboardId}`
+        `/api/${params.storeId}/products/${params.productId}`
       );
       router.refresh();
-      router.push(`/${params.storeId}/billboards`);
-      toast.success("Billboard deleted.");
+      router.push(`/${params.storeId}/products`);
+      toast.success("Product deleted.");
     } catch (error) {
       toast.error("Make sure you removed all categories first.");
     } finally {
