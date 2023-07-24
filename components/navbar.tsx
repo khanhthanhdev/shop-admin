91dacb9cd58c6cd428293e0883442d0b07269ef4
khanhthanhdev <thanhkt27507@gmail.com>
2023-07-24 09:18:17 +0700

    dark mode

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,6 +4,8 @@ import {MainNav} from '@/components/main-nav'
 import StoreSwitcher from '@/components/store-switcher'
 import { redirect } from 'next/navigation'
 import prismadb from '@/lib/prismadb'
+import { ThemeProvider } from '@/providers/theme-provider'
+import { ThemeToggle } from './theme-toggle'
 
 const Navbar = async () => {
 
@@ -25,6 +27,7 @@ const Navbar = async () => {
             <StoreSwitcher items={stores} />
             <MainNav className='mx-6' />
             <div className='ml-auto flex items-center space-x-4'>
+                <ThemeToggle />
                 <UserButton afterSignOutUrl='/' />
             </div>
         </div>
